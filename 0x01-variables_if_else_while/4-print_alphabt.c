a66113fc7cf39ca4f1e823173b14422301680aab
EngineerDorothy <dorothykerubo92@gmail.com>
2022-09-09 14:20:48 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,35 @@
  */
 
 int main(void)
+
 {
+
 	int ch;
+
+
+
 	for (ch = 'a'; ch <= 'z'; ch++)
+
 	{
+
 		if (ch != 'e' && ch != 'q')
+
 		{
+
 			putchar(ch);
+
 		}
+
 		else
+
 		{
+
 		}
+
 	}
+
 	putchar('\n');
+
 	return (0);
+
 }
