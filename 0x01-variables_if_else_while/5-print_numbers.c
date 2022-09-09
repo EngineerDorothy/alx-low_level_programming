6a4c26e5db23b878d219f0b5ab38b56a5981c557
EngineerDorothy <dorothykerubo92@gmail.com>
2022-09-09 14:08:39 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,25 @@
  * base 10 from 0
  * Return: Always 0 (success)
  */
+
 int main(void)
+
 {
+
 	int a;
+
+
+
 	for (a = 0; a < 10; a++)
+
 	{
+
 		printf("%d", a);
+
 	}
+
 	putchar('\n');
+
 	return (0);
+
 }
