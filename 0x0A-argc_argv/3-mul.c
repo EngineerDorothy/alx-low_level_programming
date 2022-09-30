6017892cd1e17a6f3066c96b9c08952a6ff7b7a4
EngineerDorothy <dorothykerubo92@gmail.com>
2022-09-30 12:48:11 -0700

    multiply 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,21 +2,19 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers
- * @argc: count
- * @argv: array
+ * main - print the result of the multiplication, followed by a new line
+ * @argc: int
+ * @argv: list
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	if (argc < 2)
+	(void)argc;
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	}
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
