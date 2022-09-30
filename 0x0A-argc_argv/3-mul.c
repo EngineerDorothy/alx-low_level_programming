71d025ece52042006d4af0af2c55f8915b08ec7e
EngineerDorothy <dorothykerubo92@gmail.com>
2022-09-30 12:25:28 -0700

    multiply 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
 /**
@@ -8,32 +7,16 @@
  * @argv: array
  * Return: 0
  */
-
-
 int main(int argc, char *argv[])
-
 {
-
-
-
 	if (argc < 2)
-
 	{
-
 		printf("Error\n");
-
 		return (1);
-
 	}
-
 	else
-
 	{
-
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
 	}
-
 	return (0);
-
 }
