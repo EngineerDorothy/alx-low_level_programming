d42558f514223ec0f5005de3681e4e67fba4e688
EngineerDorothy <dorothykerubo92@gmail.com>
2022-10-11 17:27:16 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+
+
+/**
+ *dog_t - function name
+ *@name: dog name
+ *@age: dog age
+ *@owner: owners name
+ */
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+
+{
+
+
+
+}
